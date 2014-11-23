64a30787816f111fdc18daab3eb1b6ec71dbbdf2
Jan Haller <bromeon@gmail.com>
2014-11-23 19:49:48 +0100

    [Meta] Added aurora::foreach() for iteration through variadic parameter pack

--- include/Aurora/Meta/Variadic.hpp
+++ include/Aurora/Meta/Variadic.hpp
@@ -54,12 +54,104 @@ namespace detail
 		static const std::size_t value = 0;
 	};
 
+
+	// Metafunction to apply a class template 'Function' to each type of variadic pack typelist
+	template <template <typename> class Function, typename... Ts>
+	struct ForEachType;
+
+	template <template <typename> class Function, typename T, typename... Ts>
+	struct ForEachType<Function, T, Ts...>
+	{
+		static void apply()
+		{
+			Function<T>::apply();
+			ForEachType<Function, Ts...>::apply();
+		}
+	};
+
+	template <template <typename> class Function>
+	struct ForEachType<Function>
+	{
+		static void apply()
+		{
+		}
+	};
+
+
+	// Metafunction to apply a class template 'Function' to each value of variadic pack
+	template <template <typename> class Function, typename... Ts>
+	struct ForEachValue;
+
+	template <template <typename> class Function, typename T, typename... Ts>
+	struct ForEachValue<Function, T, Ts...>
+	{
+		static void apply(T&& t, Ts&&... ts)
+		{
+			Function<T>::apply(std::forward<T>(t));
+			ForEachValue<Function, Ts...>::apply(std::forward<Ts>(ts)...);
+		}
+	};
+
+	template <template <typename> class Function>
+	struct ForEachValue<Function>
+	{
+		static void apply()
+		{
+		}
+	};
+
 } // namespace detail
 
 
 /// @addtogroup Meta
 /// @{
 
+
+/// @brief Apply function for each type in variadic parameter pack
+/// @tparam Function Class template with a static member function <b>void apply()</b>.
+/// @tparam Ts Typelist to iterate through.
+/// @details For each type @a T in @a Ts, the expression <b>Function<T>::apply()</b> is evaluated. Example:
+/// @code
+/// template <typename T>
+/// struct Example
+/// {
+///     static void apply()
+///     {
+///         std::cout << typeid(T).name() << ' ';
+///     }
+/// };
+///
+/// foreach<Example, int, float>(); // output: "int float"
+/// @endcode
+template <template <typename T> class Function, typename... Ts>
+void foreach()
+{
+	detail::ForEachType<Function, Ts...>::apply();
+}
+
+/// @brief Apply function for each value in variadic parameter pack
+/// @tparam Function Class template with a static member function <b>void apply(T& value)</b>. The parameter needs to be compatible
+///   to type @a T, however pass by value or by (const) (lvalue|rvalue) reference is allowed.
+/// @tparam Ts Argument types -- can be deduced from the function arguments.
+/// @param args Arguments to iterate through.
+/// @details For each argument @a arg in @a args, the expression <b>Function<T>::apply(arg)</b> is evaluated. Example:
+/// @code
+/// template <typename T>
+/// struct Example
+/// {
+///     static void apply(const T& arg)
+///     {
+///         std::cout << typeid(T).name() << '=' << arg << ' ';
+///     }
+/// };
+///
+/// foreach<Example>(4, 3.25f); // output: "int=4 float=3.25"
+/// @endcode
+template <template <typename T> class Function, typename... Ts>
+void foreach(Ts&&... args)
+{
+	detail::ForEachValue<Function, Ts...>::apply(std::forward<Ts>(args)...);
+}
 
 /// @}
 
