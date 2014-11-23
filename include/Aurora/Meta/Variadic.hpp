f8170de38b6f27fa7d5fdf4f0cdcc51fa24b17de
Jan Haller <bromeon@gmail.com>
2014-11-23 21:10:13 +0100

    [Meta] Made aurora::foreach() dynamic and stateful
    Now it is possible to pass functors with template operator() or C++14 generic lambdas. In contrast to stateless classes with static apply() methods, functors can carry information about their context.

--- include/Aurora/Meta/Variadic.hpp
+++ include/Aurora/Meta/Variadic.hpp
@@ -31,6 +31,8 @@
 
 #include <Aurora/Config.hpp>
 
+#include <utility> // std::forward()
+
 #ifdef AURORA_HAS_VARIADIC_TEMPLATES
 
 namespace aurora
@@ -56,46 +58,52 @@ namespace detail
 
 
 	// Metafunction to apply a class template 'Function' to each type of variadic pack typelist
-	template <template <typename> class Function, typename... Ts>
+	template <typename Function, typename... Ts>
 	struct ForEachType;
 
-	template <template <typename> class Function, typename T, typename... Ts>
+	template <typename Function, typename T, typename... Ts>
 	struct ForEachType<Function, T, Ts...>
 	{
-		static void apply()
+		static void apply(Function&& fn)
 		{
-			Function<T>::apply();
-			ForEachType<Function, Ts...>::apply();
+			// Note: Calling member operator template with explicit template argument is not too elegant
+			// Possible alternative is aurora::Type<T> argument.
+#ifdef _MSC_VER
+			fn.operator()<T>();
+#else
+			fn.template operator()<T>();
+#endif
+			ForEachType<Function, Ts...>::apply(std::forward<Function>(fn));
 		}
 	};
 
-	template <template <typename> class Function>
+	template <typename Function>
 	struct ForEachType<Function>
 	{
-		static void apply()
+		static void apply(Function&& /*fn*/)
 		{
 		}
 	};
 
 
 	// Metafunction to apply a class template 'Function' to each value of variadic pack
-	template <template <typename> class Function, typename... Ts>
+	template <typename Function, typename... Ts>
 	struct ForEachValue;
 
-	template <template <typename> class Function, typename T, typename... Ts>
+	template <typename Function, typename T, typename... Ts>
 	struct ForEachValue<Function, T, Ts...>
 	{
-		static void apply(T&& t, Ts&&... ts)
+		static void apply(Function&& fn, T&& t, Ts&&... ts)
 		{
-			Function<T>::apply(std::forward<T>(t));
-			ForEachValue<Function, Ts...>::apply(std::forward<Ts>(ts)...);
+			fn(std::forward<T>(t));
+			ForEachValue<Function, Ts...>::apply(std::forward<Function>(fn), std::forward<Ts>(ts)...);
 		}
 	};
 
-	template <template <typename> class Function>
+	template <typename Function>
 	struct ForEachValue<Function>
 	{
-		static void apply()
+		static void apply(Function&& /*fn*/)
 		{
 		}
 	};
@@ -108,49 +116,48 @@ namespace detail
 
 
 /// @brief Apply function for each type in variadic parameter pack
-/// @tparam Function Class template with a static member function <b>void apply()</b>.
 /// @tparam Ts Typelist to iterate through.
-/// @details For each type @a T in @a Ts, the expression <b>Function<T>::apply()</b> is evaluated. Example:
+/// @param fn Function object with a member function template <b>void operator() ()</b>.
+/// @details For each type @a T in @a Ts, the @a fn's operator() is called with explicit template argument @a T. Example:
 /// @code
-/// template <typename T>
 /// struct Example
 /// {
-///     static void apply()
+///     template <typename T>
+///     void operator() ()
 ///     {
 ///         std::cout << typeid(T).name() << ' ';
 ///     }
 /// };
 ///
-/// foreach<Example, int, float>(); // output: "int float"
+/// aurora::foreach<int, float>(Example()); // output: "int float"
 /// @endcode
-template <template <typename T> class Function, typename... Ts>
-void foreach()
+template <typename... Ts, typename Function>
+void foreach(Function&& fn)
 {
-	detail::ForEachType<Function, Ts...>::apply();
+	detail::ForEachType<Function, Ts...>::apply(std::forward<Function>(fn));
 }
 
 /// @brief Apply function for each value in variadic parameter pack
-/// @tparam Function Class template with a static member function <b>void apply(T& value)</b>. The parameter needs to be compatible
-///   to type @a T, however pass by value or by (const) (lvalue|rvalue) reference is allowed.
-/// @tparam Ts Argument types -- can be deduced from the function arguments.
+/// @param fn Function object with a member function template <b>void operator() (T& value)</b>, or C++14 generic lambda. 
+///  The parameter must be compatible to type @a T, however pass by value or by (const) (lvalue|rvalue) reference is allowed.
 /// @param args Arguments to iterate through.
-/// @details For each argument @a arg in @a args, the expression <b>Function<T>::apply(arg)</b> is evaluated. Example:
+/// @details For each argument @a arg in @a args, the expression <b>fn(arg)</b> is evaluated. Example:
 /// @code
-/// template <typename T>
 /// struct Example
 /// {
-///     static void apply(const T& arg)
+///     template <typename T>
+///     void operator() (const T& arg)
 ///     {
 ///         std::cout << typeid(T).name() << '=' << arg << ' ';
 ///     }
 /// };
 ///
-/// foreach<Example>(4, 3.25f); // output: "int=4 float=3.25"
+/// aurora::foreach(Example(), 4, 3.25f); // output: "int=4 float=3.25"
 /// @endcode
-template <template <typename T> class Function, typename... Ts>
-void foreach(Ts&&... args)
+template <typename... Ts, typename Function>
+void foreach(Function&& fn, Ts&&... args)
 {
-	detail::ForEachValue<Function, Ts...>::apply(std::forward<Ts>(args)...);
+	detail::ForEachValue<Function, Ts...>::apply(std::forward<Function>(fn), std::forward<Ts>(args)...);
 }
 
 /// @}
