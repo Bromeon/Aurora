3920c5a31f3ba69809bf0b1138f1261e634b47b8
Jan Haller <bromeon@gmail.com>
2015-12-30 16:45:34 +0100

    [All] Removed trailing whitespaces

--- include/Aurora/Meta/Templates.hpp
+++ include/Aurora/Meta/Templates.hpp
@@ -2,23 +2,23 @@
 //
 // Aurora C++ Library
 // Copyright (c) 2012-2015 Jan Haller
-// 
+//
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
 // arising from the use of this software.
-// 
+//
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute it
 // freely, subject to the following restrictions:
-// 
+//
 // 1. The origin of this software must not be misrepresented; you must not
 //    claim that you wrote the original software. If you use this software
 //    in a product, an acknowledgment in the product documentation would be
 //    appreciated but is not required.
-// 
+//
 // 2. Altered source versions must be plainly marked as such, and must not be
 //    misrepresented as being the original software.
-// 
+//
 // 3. This notice may not be removed or altered from any source distribution.
 //
 /////////////////////////////////////////////////////////////////////////////////
@@ -113,7 +113,7 @@ namespace detail
 
 /// @addtogroup Meta
 /// @{
-	
+
 /// @brief Simple type wrapper, can be used for overload resolution.
 ///
 template <typename T>
@@ -215,7 +215,7 @@ struct FunctionArity
 /// @}
 
 // ---------------------------------------------------------------------------------------------------------------------------
-	
+
 
 namespace detail
 {
@@ -258,7 +258,7 @@ namespace detail
 
 	// Human-readable form
 	#define AURORA_REPLICATE(Origin, New) typename aurora::detail::Replicate<Origin, New>::type
-	
+
 } // namespace detail
 } // namespace aurora
 
